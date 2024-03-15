c2f1abd238e2882be34de73110e8502e8f297275
Mengqi Pei <mengqipei@gmail.com>
2024-03-15 19:56:09 +0800

    Remove showing anchor icon for all the links

--- src/app/_components/MarkdownPost.tsx
+++ src/app/_components/MarkdownPost.tsx
@@ -6,7 +6,7 @@ const components: MDXComponents = {
     // Allows customizing built-in components, e.g. to add styling.
     h1: ({ children }) => <h1 style={{ fontSize: '100px' }}>{children}</h1>,
     a: ({ children, href }) => (
-        <Link href={href} color="secondary" showAnchorIcon>{children}</Link>
+        <Link href={href} color="secondary">{children}</Link>
     )
 };
 
