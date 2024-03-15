9df8a2c36a61db033cf34608e2abb391fc64eba5
Mengqi Pei <mengqipei@gmail.com>
2024-03-15 18:22:16 +0800

    Minor update

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -6,15 +6,6 @@ export function Footer() {
       <p className="text-gray-500">
         © 2015 - 2024 Mengqi | All rights reserved.</p>
       <div className="grid gap-2 md:flex md:gap-4">
-        <Link className="text-gray-500 hover:underline" href="#">
-          Terms
-        </Link>
-        <Link className="text-gray-500 hover:underline" href="#">
-          Privacy
-        </Link>
-        <Link className="text-gray-500 hover:underline" href="#">
-          Policy
-        </Link>
         <span id="busuanzi_container_site_pv">本站总访问量<span id="busuanzi_value_site_pv"></span>次</span>
         <span id="busuanzi_container_site_uv">
           本站访客数<span id="busuanzi_value_site_uv"></span>人次
