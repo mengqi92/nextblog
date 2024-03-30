b315ac519598eb9cb8fdcfebf61829753449357c
Mengqi Pei <mengqipei@gmail.com>
2024-03-30 18:00:48 +0800

    Update code style

--- src/app/_components/markdown-post.tsx
+++ src/app/_components/markdown-post.tsx
@@ -1,18 +1,26 @@
 import { useMDXComponent } from "next-contentlayer/hooks";
 import { MDXComponents } from "mdx/types";
+import clsx from "clsx";
 
 const components: MDXComponents = {
     // Allows customizing built-in components, e.g. to add styling.
     h1: ({ children }) => <h1 className="my-4 scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">{children}</h1>,
     h2: ({ children }) => <h2 className="my-4 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">{children}</h2>,
-    h3: ({children}) => <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">{children}</h3>,
-    h4: ({children}) => <h4 className="my-4 scroll-m-20 text-xl font-semibold tracking-tight">{children}</h4>,
-    p: ({children}) => <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>,
-    blockquote: ({children}) => <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>,
-    ul: ({children}) => <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>,
-    li: ({children}) => <li className="leading-7 list-outside list-decimal">{children}</li>,
-    code: ({children}) => <code className="relative rounded my-2 px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">{children}</code>,
-    lead: ({children}) => <p className="text-xl text-muted-foreground">{children}</p>,
+    h3: ({ children }) => <h3 className="my-4 scroll-m-20 text-2xl font-semibold tracking-tight">{children}</h3>,
+    h4: ({ children }) => <h4 className="my-4 scroll-m-20 text-xl font-semibold tracking-tight">{children}</h4>,
+    p: ({ children }) => <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>,
+    blockquote: ({ children }) => <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>,
+    ul: ({ children }) => <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>,
+    li: ({ children }) => <li className="leading-7 list-outside list-decimal">{children}</li>,
+    pre: ({ className, children, ...props }) => {
+        return (
+            <pre className={clsx("p-0 my-4 px-4 py-4 bg-transparent rounded", className)} {...props}>
+                {children}
+            </pre>
+        )
+    },
+    code: ({children}) => <code className="relative my-4 px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">{children}</code>,
+    lead: ({ children }) => <p className="text-xl text-muted-foreground">{children}</p>,
     a: ({ children, href }) => (
         <a href={href} className="text-red-500 hover:text-red-700">{children}</a>
     )
