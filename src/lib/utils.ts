1218ef2d2b6be42cd15c1d6b0e18d3802bcbcc58
Mengqi Pei <mengqipei@gmail.com>
2024-03-30 13:42:48 +0800

    Add Atom RSS support

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,33 +2,41 @@ import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 import { allPosts } from "contentlayer/generated";
 import RSS from 'rss';
+import { Feed } from 'feed';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export async function generateRssFeed(): Promise<RSS> {
+export async function generateRssFeed(): Promise<string> {
   const site_url = 'https://mengqi92.github.io'
 
   const feedOptions = {
     title: 'Blog posts | RSS Feed',
     description: 'Welcome to this blog posts!',
-    site_url: site_url,
-    feed_url: `${site_url}/rss.xml`,
-    image_url: `${site_url}/logo.png`,
-    pubDate: new Date(),
+    id: site_url,
+    link: site_url,
+    image: `${site_url}/logo.png`,
+    favicon: `${site_url}/favicon.png`,
     copyright: `All rights reserved ${new Date().getFullYear()}, Ibas`,
+    generator: 'Feed for Node.js',
+    feedLinks: {
+      rss2: `${site_url}/rss.xml`,
+      atom: `${site_url}/atom.xml`
+    }
   };
-  const feed = new RSS(feedOptions);
+  const feed = new Feed(feedOptions);
 
   allPosts.map((post) => {
-    feed.item({
+    feed.addItem({
       title: post.title,
+      id: `${site_url}/posts/${post.slugAsParams}`,
+      link: `${site_url}/posts/${post.slugAsParams}`,
       description: post.description,
-      url: post.url,
-      date: post.createdDate
+      date: new Date(post.modifiedDate),
+      published: new Date(post.createdDate),
     });
   })
 
-  return feed;
+  return feed.atom1();
 }
