cfd0976edaa9fefa272d6a94b38bdcb1e0b9ffd2
Mengqi Pei <mengqipei@gmail.com>
2024-03-30 15:23:33 +0800

    Add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { withContentlayer } from 'next-contentlayer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     // show remote images
     images: {
         remotePatterns: [
@@ -12,8 +13,18 @@ const nextConfig = {
             },
         ],
     },
-
     pageExtensions: ['js', 'jsx', 'mdx', 'md', 'ts', 'tsx'],
+    async redirects() {
+        return [
+            {
+                source: '/:year(\\d{4})/:month(\\d{2})/:day(\\d{2})/:slug',
+                destination: '/posts/:slug',
+                basePath: false,
+                permanent: true,
+            },
+        ]
+    },
 }
 
-export default withContentlayer(nextConfig);
+// export default withContentlayer(nextConfig);
+export default nextConfig;
